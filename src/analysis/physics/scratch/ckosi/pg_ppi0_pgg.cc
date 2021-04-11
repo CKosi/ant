1cff2e223c5fbd246730a04bfb27691b2600f1ca
Ckosi@students.uni-mainz.de <Ckosi@students.uni-mainz.de>
2021-04-11 12:58:19 +0200

    Primakoff alp class added gp->ap->ggp

--- src/analysis/physics/scratch/ckosi/pg_ppi0_pgg.cc
+++ src/analysis/physics/scratch/ckosi/pg_ppi0_pgg.cc
@@ -1,19 +1,30 @@
 #include "pg_ppi0_pgg.h"
 #include "TLorentzVector.h"
+#include "analysis/utils/fitter/KinFitter.h"
+#include "utils/uncertainties/Interpolated.h"
+#include "analysis/utils/uncertainties/FitterSergey.h"
 
 using namespace std;
 using namespace ant;
 using namespace ant::analysis;
 using namespace ant::analysis::physics;
 
-scratch_kosi_pg_ppi0g_pgg::scratch_kosi_pg_ppi0g_pgg(const string& name, OptionsPtr opts) :
-    Physics(name, opts)
+scratch_kosi_pg_ppi0_pgg::scratch_kosi_pg_ppi0_pgg(const string& name, OptionsPtr opts) :
+    Physics(name, opts),
+  fit_model(utils::UncertaintyModels::Interpolated::makeAndLoad(
+              utils::UncertaintyModels::Interpolated::Type_t::MC,
+              make_shared<utils::UncertaintyModels::FitterSergey>())),
+  fitter(nullptr, opts->Get<bool>("FitZVertex", true))
 {
     const BinSettings tagger_time_bins(2000, -200, 200);
-    const BinSettings theta_time_bins(180, 0, 180);
+    const BinSettings theta_time_bins(100, -1, 1);
     const BinSettings numcands_time_bins(10, 0, 10);
-    const BinSettings theta_pi0_bins(180, 0, 180);
-    const BinSettings theta_pi0Zoom_bins(25, 0, 5);
+    const BinSettings theta_pi0_bins(100, -1, 1);
+    const BinSettings gamma_im_bins(1000, 0, 1000);
+    const BinSettings gamma_mm_bins(1500, 0, 1500);
+    const BinSettings energy_bins(88,1.07352,1.51254);
+
+    fitter.SetZVertexSigma(3.0);
 
         h_TaggerTime = HistFac.makeTH1D("Tagger Time",     // title
                                         "t [ns]", "#",     // xlabel, ylabel
@@ -21,103 +32,129 @@ scratch_kosi_pg_ppi0g_pgg::scratch_kosi_pg_ppi0g_pgg(const string& name, Options
                                         "h_TaggerTime"     // ROOT object name, auto-generated if omitted
                                         );
 
-        h_ThetaDist = HistFac.makeTH1D("Theta distribution",
-                                       "Angle [°]", "#",
-                                       theta_time_bins,
-                                       "h_ThetaDist");
-
         h_NumCands = HistFac.makeTH1D("Number of candidates",
                                        "Candidates", "#",
                                        numcands_time_bins,
                                        "h_NumCands");
 
         h_ThetaPi0 = HistFac.makeTH1D("Theta of Pi0",
-                                      "Angle [°]", "#",
+                                      "cos_Theta [deg]", "#",
                                       theta_pi0_bins,
                                       "h_ThetaPi0");
 
-        h_ThetaPi0Zoom = HistFac.makeTH1D("Theta of Pi0Zommed",
-                                      "Angle [°]", "#",
-                                      theta_pi0Zoom_bins,
-                                      "h_ThetaPi0Zoom");
+        h_ThetaPi0Cut = HistFac.makeTH1D("Theta of Pi0 after cut",
+                                      "cos_Theta [deg]", "#",
+                                      theta_pi0_bins,
+                                      "h_ThetaPi0Cut");
+
+        h_GammaIM = HistFac.makeTH1D("Invariant mass of decay gammas",
+                                      "Mass [MeV]", "#",
+                                      gamma_im_bins,
+                                      "h_GammaIM");
+
+        h_MM = HistFac.makeTH1D("Missing mass",
+                                      "Mass [MeV]", "#",
+                                      gamma_mm_bins,
+                                      "h_GammaMM");
+
+        h_cs = HistFac.makeTH2D("Cross section",
+                                "cos_Theta [deg]","sqrt_S [GeV]",
+                                theta_pi0_bins, energy_bins,    // our binnings
+                                "h_cs",false);
 }
 
-void scratch_kosi_pg_ppi0g_pgg::ProcessEvent(const TEvent& event, manager_t&)
+void scratch_kosi_pg_ppi0_pgg::ProcessEvent(const TEvent& event, manager_t&)
 {
     const auto& cands = event.Reconstructed().Candidates;
     TrigSim.ProcessEvent(event);
+    const double weight = promptrandom.FillWeight();
 
-    TCandidatePtrList gammas;
-    TCandidatePtrList protons;
+    fitter.SetUncertaintyModel(fit_model);
 
-    vector<double> protonCalE;
-    vector<double> gammaCalE;
+    TCandidatePtrList neutral;
+    TCandidatePtrList charged;
 
-    TParticle pGammas[num_gammas];
-    TParticle pProton[num_protons];
+    TParticleList protons;
+    TParticleList photons;
+    TParticlePtr proton;
 
-    //Loop over candidates
+    //Loop over candidates and distinguish between charged and neutral
 
     for (const auto& c : cands.get_iter()) {
-        if (c->VetoEnergy <= vetoEthreshold){
-            gammas.emplace_back(c);
-            gammaCalE.push_back(c->CaloEnergy);
-        }else{
-            protons.emplace_back(c);
-            protonCalE.push_back(c->CaloEnergy);
+        if (c->VetoEnergy <= vetoEthreshold){ neutral.emplace_back(c);
+         photons.emplace_back(std::make_shared<TParticle>(ParticleTypeDatabase::Photon, c));
+        }
+        else charged.emplace_back(c);
+        protons.emplace_back(std::make_shared<TParticle>(ParticleTypeDatabase::Proton, c));
         }
 
-        h_ThetaDist->Fill(c->Theta*radtodeg);        //Thetas of all candidates
-        }
 
-    if(cands.size() == 3){num_3decay += 1;}
-    h_NumCands->Fill(cands.size());                 //Distribution of candidate numbers
+    h_NumCands->Fill(cands.size());
 
 
-    //Loop over taggerhits
-    for (const auto& taggerhit : event.Reconstructed().TaggerHits) {
+    //Check if decay particles are 2 gammas Pi0->gg
+    if(neutral.size() == 2){
 
-        //Check if decay particles are 2 gammas and 1 proton
-        if(gammaCalE.size() == num_gammas && protonCalE.size() == num_protons){
-            for(int i = 0; i<num_gammas; i++){
-                pGammas[i] = TParticle(ParticleTypeDatabase::Photon, gammas[i]);
-            }
-            for(int i = 0; i<num_protons; i++){
-                pProton[i] = TParticle(ParticleTypeDatabase::Proton, protons[0]);
+        //Create Pi0
+        TParticle Pi0;
+
+
+        //Loop over taggerhits
+        for (const auto& taggerhit : event.Reconstructed().TaggerHits) {
+
+            for(const auto& photon : neutral){
+                Pi0 += TParticle(ParticleTypeDatabase::Photon, photon);
             }
 
-            //Get vector of pi0
-            TLorentzVector pi0_decay = pGammas[0] + pGammas[1];
+            h_TaggerTime->Fill(taggerhit.Time,weight);
 
-            TParticle pPi0 = TParticle(ParticleTypeDatabase::Pi0,(TLorentzVector)(pi0_decay));
-            //Fill with theta of pi0
-            h_ThetaPi0->Fill(pPi0.Theta()*radtodeg);
+            //Initial conditions
+            auto initial = taggerhit.GetPhotonBeam() + LorentzVec(vec3(0,0,0),ParticleTypeDatabase::Proton.Mass());
+            auto missingMass = initial - Pi0;
+
+//            if (missingMass.M() > 990) continue;
+//            if (missingMass.M() < 880) continue;
+
+            h_GammaIM->Fill(Pi0.M(), weight);
+            h_MM->Fill(missingMass.M(),weight);
+            h_ThetaPi0->Fill(cos(Pi0.Theta()),weight);
+            h_cs->Fill(cos(Pi0.Theta()),initial.M()/1000,weight);
+
+            //Cut away everything thats Cos(°) < 0.9
+
+            if(cos(Pi0.Theta()) >= 0.9){
+
+               h_ThetaPi0Cut->Fill(cos(Pi0.Theta()),weight);
+            }
         }
-
-        h_TaggerTime->Fill(taggerhit.Time);                         //Tagger times
-        }
-
+    }
 }
 
 
-void scratch_kosi_pg_ppi0g_pgg::ShowResult()
+void scratch_kosi_pg_ppi0_pgg::ShowResult()
 {
     ant::canvas(GetName()+": Basic plots")
             << h_TaggerTime
-            << h_ThetaDist
             << h_NumCands
             << endc; // actually draws the canvas
 
     ant::canvas(GetName()+": Pi0 Dist")
             << h_ThetaPi0
-            << endc; // actually draws the canvas
+            << h_ThetaPi0Cut
+            << h_GammaIM
+            << h_MM
+            << endc;
+
+    ant::canvas(GetName()+": cross section")
+            << drawoption("Surf")
+            << h_cs
+            << endc;
 
 }
 
-void scratch_kosi_pg_ppi0g_pgg::Finish()
+void scratch_kosi_pg_ppi0_pgg::Finish()
 {
     cout<<"Finished"<<endl;
-    cout<<"Number of events with 3 decay products "<<num_3decay<<endl;
 }
 
-AUTO_REGISTER_PHYSICS(scratch_kosi_pg_ppi0g_pgg)
+AUTO_REGISTER_PHYSICS(scratch_kosi_pg_ppi0_pgg)
--- src/analysis/physics/scratch/ckosi/pg_ppi0_pgg.h
+++ src/analysis/physics/scratch/ckosi/pg_ppi0_pgg.h
@@ -3,35 +3,39 @@
 #include "physics/Physics.h"
 #include "plot/PromptRandomHist.h"
 #include "utils/TriggerSimulation.h"
+#include "utils/fitter/KinFitter.h"
+#include "analysis/utils/Uncertainties.h"
 
 namespace ant {
 namespace analysis {
 namespace physics {
 
-class scratch_kosi_pg_ppi0g_pgg : public Physics {
+class scratch_kosi_pg_ppi0_pgg : public Physics {
 public:
-    scratch_kosi_pg_ppi0g_pgg (const std::string& name, OptionsPtr opts);
+    scratch_kosi_pg_ppi0_pgg (const std::string& name, OptionsPtr opts);
     virtual void ProcessEvent(const TEvent& event, manager_t& manager) override;
     virtual void ShowResult() override;
     virtual void Finish() override;
 
 protected:
     static constexpr auto radtodeg = std_ext::radian_to_degree(1.0);
+    utils::UncertaintyModelPtr fit_model;
+    utils::KinFitter fitter;
+
 private:
     TH1D* h_nClusters;
     TH1D* h_TaggerTime;
-    TH1D* h_ThetaDist;
     TH1D* h_NumCands;
     TH1D* h_ThetaPi0;
-    TH1D* h_ThetaPi0Zoom;
+    TH1D* h_ThetaPi0Cut;
+    TH1D* h_GammaIM;
+    TH1D* h_MM;
+    TH2D* h_cs;
 
     double vetoEthreshold = 0.1;
-    static const int num_gammas = 2;
-    static const int num_protons = 1;
-
-    unsigned int num_3decay;
 
     utils::TriggerSimulation TrigSim;
     PromptRandom::Switch promptrandom;
+
 };
 }}}
